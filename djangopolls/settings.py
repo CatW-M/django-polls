eaab5a304880f1f4936d34f3c95be2c8d5110825
CatW-M <wheelermushlin@gmail.com>
2022-11-30 23:08:49 -0700

    feat: updated question choice models and admin settings

--- djangopolls/settings.py
+++ djangopolls/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'djangopolls.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'pollsdjango',
+        'NAME': 'polls',
     }
 }
 
